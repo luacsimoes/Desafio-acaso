2e476da281c71c497ef21f7e0f8cb2b9ebc634b5
luana simões <simoesluana2001@gmail.com>
2023-04-13 22:02:59 -0300

    add params

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
-import styled from 'styled-components/native';
-import theme, { MyTheme } from '@/Theme/Theme';
+import { TouchableOpacityProps } from 'react-native';
+import theme from '@/Theme/Theme';
+import { ButtonContainer, ButtonText } from './styles';
 
 interface ButtonProps extends TouchableOpacityProps {
   onPress: () => void;
@@ -10,35 +10,6 @@ interface ButtonProps extends TouchableOpacityProps {
   isWhite?: boolean;
   children: React.ReactNode;
 }
-
-interface ButtonContainerProps {
-  isWhite?: boolean;
-  width: number;
-  height: number;
-  theme: MyTheme;
-}
-
-const ButtonContainer = styled(TouchableOpacity)<ButtonContainerProps>`
-  width: ${({ width }) => width}px;
-  height: ${({ height }) => height}px;
-  border-radius: ${({ height }) => height / 2}px;
-  align-items: center;
-  justify-content: center;
-  background-color: ${({ isWhite, theme }) =>
-    isWhite ? theme.colors.secondary : theme.colors.primary};
-`;
-
-interface ButtonTextProps {
-  isWhite?: boolean;
-  theme: MyTheme;
-}
-
-const ButtonText = styled.Text<ButtonTextProps>`
-  color: ${({ isWhite, theme }) =>
-    isWhite ? theme.colors.primary : theme.colors.secondary};
-  font-size: 14px;
-  font-weight: bold;
-`;
 
 const Button = ({
   onPress,
