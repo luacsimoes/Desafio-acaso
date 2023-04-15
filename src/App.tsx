615546f158310f32d9e58e37442db75f1f6d73ce
luana simões <simoesluana2001@gmail.com>
2023-04-15 00:12:15 -0300

    feed provider updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -6,44 +6,42 @@ import Toast from 'react-native-toast-message';
 import ThemeProvider from './Theme/ThemeProvider';
 import { AuthProvider } from './context/Auth';
 import Route from './routes';
+import FeedProvider from './context/Feed';
 import { toastConfig } from './Toast/toastConfig';
 import { BASE_URL } from './config';
 
 export const App = () => {
-  const defineInterceptor = () => {
-    axios.interceptors.response.use(
-      (response) => {
-        return response;
-      },
-      (err) => {
-        return new Promise((resolve, reject) => {
-          const originalReq = err.config;
-          if (err.response.status === 401 && err.config && !err.config.retry) {
-            originalReq.retry = true;
-            AsyncStorage.getItem('TOKEN').then((token) => {
-              const res = axios
-                .post(`${BASE_URL}/auth/refresh-token`, { oldToken: token })
-                .then((res) => {
-                  AsyncStorage.setItem('TOKEN', res.data.access_token);
-                  originalReq.headers.Authorization = `Bearer ${res.data.token.access_token}`;
-                  return axios(originalReq);
-                });
-              resolve(res);
-            });
-          } else {
-            reject(err);
+  useEffect(() => {
+    const interceptor = axios.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (error.response && error.response.status === 401) {
+          const originalRequest = error.config;
+          const refreshToken = await AsyncStorage.getItem('refreshToken');
+
+          if (refreshToken) {
+            axios.defaults.headers.common.Authorization = `Bearer ${refreshToken}`;
+            const response = await axios.post(`${BASE_URL}/auth/refresh`, {});
+            AsyncStorage.setItem('token', response.data.token);
+            AsyncStorage.setItem('refreshToken', response.data.refreshToken);
+            axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`;
+            originalRequest.headers.Authorization = `Bearer ${response.data.token}`;
+            return axios(originalRequest);
           }
-        });
+        }
+        return Promise.reject(error);
       },
     );
-  };
-  defineInterceptor();
+    return () => axios.interceptors.response.eject(interceptor);
+  }, []);
 
   return (
     <ThemeProvider>
       <NavigationContainer>
         <AuthProvider>
-          <Route />
+          <FeedProvider>
+            <Route />
+          </FeedProvider>
         </AuthProvider>
       </NavigationContainer>
       <Toast config={toastConfig} />
