4999dd7b3c690824137b17fc872961b115a7460d
luana simões <simoesluana2001@gmail.com>
2023-04-14 23:28:34 -0300

    try to insert profile picture

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { Text, Image } from 'react-native';
+import { AuthContext } from '@/context/Auth';
+import { Container, Header, AcasoLogo, ProfilePicture } from './styles';
 
-const Feed = () => {
-  return <Text>oi</Text>;
+const Feed = ({ userPicture }: { userPicture: string }) => {
+  const [picture, setPicture] = useState<string>(userPicture);
+
+  return (
+    <Container>
+      <Header>
+        <AcasoLogo source={require('./images/logo.png')} />
+        {picture !== '' ? (
+          <ProfilePicture source={{ uri: userPicture }} />
+        ) : (
+          <ProfilePicture source={require('./images/defaultimage.jpg')} />
+        )}
+      </Header>
+    </Container>
+  );
 };
 
 export default Feed;
